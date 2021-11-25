b21afacfc795d136334c9057b55f5ecbfb70a2a4
KWollscheid <Klaus.Wollscheid@t-online.de>
2021-11-25 17:42:13 +0100

    Gitignore and predict changes

--- src/predict.py
+++ src/predict.py
@@ -8,5 +8,13 @@ mpg = pickle.load(open("data/models/model.pickle", "rb"))
 # zylinder,ps,gewicht,beschleunigung,baujahr
 x_query = np.array([8, 240, 3000, 10, 70])
 
-print("Ein Auto mit {} Zylindern, {} PS, {} kg, Beschleunigung von {}(?), Baujahr 19{} wird vermutlich {} l/100km verbrauchen".format(
-    x_query[0], x_query[1], x_query[2]*0.453592, x_query[3], x_query[4], (235.215/(mpg.predict(x_query.reshape(1, -1)))[0]).round(2)))
+string = ("Ein Auto mit {} Zylindern, {} PS, {} kg, Beschleunigung von {}(?)," +
+          "Baujahr 19{} wird vermutlich {} l/100km verbrauchen")
+
+print(
+    string.format(
+        x_query[0], x_query[1], x_query[2] *
+        0.453592, x_query[3], x_query[4],
+        (235.215/(mpg.predict(x_query.reshape(1, -1)))[0]).round(2)
+    )
+)
